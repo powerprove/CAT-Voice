daec3e827b7d85802e8012fd9615a7e7027af384
powerprove <blanknhide@naver.com>
2021-04-18 15:06:46 +0900

    [+] ADD ROOMCOMMAND

---
+++ src/Server/User.java
@@ -0,0 +1,39 @@
+package Server;
+
+public class User
+{
+    private String nickName;
+    private String statusMessage;
+
+    User()
+    {
+        nickName = null;
+        statusMessage = null;
+    }
+
+    User(String nickName,String statusMessage)
+    {
+        this.nickName = nickName;
+        setStatusMessage(statusMessage);
+    }
+
+    public String getNickName()
+    {
+        return nickName;
+    }
+
+    public String getStatusMessage()
+    {
+        return statusMessage;
+    }
+
+    public void setNickname(String nickName)
+    {
+        this.nickName = nickName;
+    }
+
+    public void setStatusMessage(String statusMessage)
+    {
+        this.statusMessage = statusMessage;
+    }
+}
